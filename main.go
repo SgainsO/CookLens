6d876ee3a668994f2b57c261dcc13dbcd10e7a59
SgainsO <jptjasc@gmail.com>
2025-09-04 14:52:03 -0400

    Finished backend implementation

--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fmt"
 	"strings"
+
 	"github.com/gocolly/colly"
-	"github.com/jbrukh/bayesian"
 )
 
-type Memory struct {
-	Amt         int8
-	Items       [3]string
-	Item_Type   [3]int8 //0  is not Ing, 1 is Ing
+type ItemHolder struct {
+	item   string
+	number int
 }
 
+type Memory struct {
+	Amt       int8
+	Items     [3]ItemHolder
+	Item_Type [3]int8 //0  is not Ing, 1 is Ing
+}
 
 func (m Memory) Amt_Correct(typeNum int8) int {
 	newAmt := 0
-	for _, value := range m.Item_Type{
+	for _, value := range m.Item_Type {
 		if value == typeNum {
 			newAmt++
 		}
@@ -27,7 +31,7 @@ func (m Memory) Amt_Correct(typeNum int8) int {
 func (m Memory) ReturnLeftovers(s []string, toRet int8) []string {
 	for index, value := range m.Items {
 		if m.Item_Type[index] == toRet {
-			s = AddToSlice(value, s)
+			s = AddToSlice(value.item, s)
 		} else {
 			break
 		}
@@ -44,26 +48,24 @@ func (m *Memory) DeepCopyMemory(toCopy Memory) {
 }
 
 func (m *Memory) ClearMemory() {
-	m.Items = [3]string{}
+	m.Items = [3]ItemHolder{}
 	m.Amt = 0
-//	fmt.Println("Memory cleared")
+	// fmt.Println("Memory cleared")
 }
 
-var memory Memory = Memory{Items: [3]string{},
-	Item_Type: [3]int8{}, Amt: 0}
-
-var Leftovers Memory = Memory{Items: [3]string{"", "", ""},
-	Item_Type: [3]int8{0, 0, 0}}
-
-var Ings []string = []string{}
-var Recipe []string = []string{}
-func main() {
+// ParseRecipeFromURL extracts ingredients and recipe steps from a URL
+// This function is safe for use in coroutines as it uses local variables
+func ParseRecipeFromURL(url string, ings *[]ItemHolder, recipe *[]ItemHolder) error {
 	AssignWordLists()
 	LoadPositives()
-	fmt.Println("Input a Link you want to find the recipe of!")
-	c := colly.NewCollector()
 
+	c := colly.NewCollector()
+	itemNumber := 0
 	leftoverPossible := false
+
+	// Local memory variables for coroutine safety
+	memory := Memory{Items: [3]ItemHolder{}, Item_Type: [3]int8{}, Amt: 0}
+	leftovers := Memory{Items: [3]ItemHolder{}, Item_Type: [3]int8{0, 0, 0}}
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
@@ -74,47 +76,73 @@ func main() {
 	})
 
 	c.OnHTML("li", func(e *colly.HTMLElement) {
+		itemNumber++
 		trimmedText := strings.TrimSpace(e.Text)
 		if trimmedText != "" {
 			if IsIngredient(trimmedText) {
-				memory.AddToMemory(trimmedText, 1)
+				memory.AddToMemory(trimmedText, 1, itemNumber)
 				fmt.Printf("%s registered!\n", trimmedText)
 			} else if IsInstruction(trimmedText) {
-				memory.AddToMemory(trimmedText, 2)
+				memory.AddToMemory(trimmedText, 2, itemNumber)
 				fmt.Printf("%s INSTRUCTION \n", trimmedText)
 			} else {
-				memory.AddToMemory(trimmedText, 0)
-			//	fmt.Printf("%s other\n", trimmedText)
+				memory.AddToMemory(trimmedText, 0, itemNumber)
 			}
 
-
-			//Always will run regardless of corectness
 			if memory.Amt == 3 {
-				if memory.Amt_Correct(1) == 3{
+				if memory.Amt_Correct(1) == 3 {
 					fmt.Println("This is running")
 					for _, item := range memory.Items {
-						Ings = AddToSlice(item, Ings)
+						*ings = AddItemToSlice(item.item, item.number, *ings)
 					}
 				}
-				if memory.Amt_Correct(2) == 3{
+				if memory.Amt_Correct(2) == 3 {
 					for _, item := range memory.Items {
-						Recipe = AddToSlice(item, Recipe)
+						*recipe = AddItemToSlice(item.item, item.number, *recipe)
 					}
 				}
 				fmt.Printf("Before entering\n")
-				leftoverPossible = handleLeftovers(leftoverPossible)
+				leftoverPossible = handleLeftoversLocal(leftoverPossible, itemNumber, &memory, &leftovers, ings, recipe)
 				memory.ClearMemory()
 			}
 		}
 	})
-	bmodel, _ := bayesian.NewClassifierFromFile("model/model.mo")
 
-	search("https://www.recipetineats.com/mexican-corn-salad/", c, bmodel)
+	c.Visit(url)
+
+	// Sort by order number to preserve original webpage order
+	sortItemHoldersByOrder(*ings)
+	sortItemHoldersByOrder(*recipe)
+
+	return nil
+}
+
+func main() {
+	var ings []ItemHolder = []ItemHolder{}
+	var recipe []ItemHolder = []ItemHolder{}
+
+	fmt.Println("Input a Link you want to find the recipe of!")
+	err := ParseRecipeFromURL("https://www.recipetineats.com/mexican-corn-salad/", &ings, &recipe)
+	if err != nil {
+		fmt.Printf("Error parsing recipe: %v\n", err)
+		return
+	}
+
+	fmt.Println("Ended search")
+	PrintAllItemHolders(ings)
+	fmt.Println("-----------------")
+	PrintAllItemHolders(recipe)
 }
 
 func PrintAllInSlice(s []string) {
 	for _, value := range s {
 		fmt.Println(value)
+	}
+}
+
+func PrintAllItemHolders(s []ItemHolder) {
+	for _, value := range s {
+		fmt.Printf("[%d] %s\n", value.number, value.item)
 	}
 }
 
@@ -124,29 +152,29 @@ func putAllInArrayIntoSlice(list []string, array []string) {
 	}
 }
 
-func handleLeftovers(leftoverSet bool) (bool) {
+func handleLeftoversLocal(leftoverSet bool, currentItemNumber int, memory *Memory, leftovers *Memory, ings *[]ItemHolder, recipe *[]ItemHolder) bool {
 
 	//This patch runs first, will always be wrong
-	fmt.Printf("%d %d %d correct", leftoverSet, memory.Amt_Correct(2), Leftovers.Amt_Correct(2))
+	fmt.Printf("%d %d %d correct", leftoverSet, memory.Amt_Correct(2), leftovers.Amt_Correct(2))
 	if leftoverSet {
 		for kind := int8(1); kind <= 2; kind++ {
 			for i := 0; i < memory.Amt_Correct(kind); i++ {
-				if Leftovers.Items[i] != "" {
+				if leftovers.Items[i].item != "" {
 					if kind == 1 {
-						Ings = AddToSlice(Leftovers.Items[i], Ings)
+						*ings = AddItemToSlice(leftovers.Items[i].item, leftovers.Items[i].number, *ings)
 					} else {
-						Recipe = AddToSlice(Leftovers.Items[i], Recipe)
+						*recipe = AddItemToSlice(leftovers.Items[i].item, leftovers.Items[i].number, *recipe)
 					}
 				}
 			}
 
-			if memory.Amt_Correct(kind) == 0 && Leftovers.Amt_Correct(kind) > 1 {
-				for i := int(Leftovers.Amt) - 1; i >= int(0); i-- {
-					if Leftovers.Items[i] != "" && Leftovers.Item_Type[i] == int8(kind) {
+			if memory.Amt_Correct(kind) == 0 && leftovers.Amt_Correct(kind) > 1 {
+				for i := int(leftovers.Amt) - 1; i >= int(0); i-- {
+					if leftovers.Items[i].item != "" && leftovers.Item_Type[i] == int8(kind) {
 						if kind == 1 {
-							Ings = AddToSlice(Leftovers.Items[i], Ings)
+							*ings = AddItemToSlice(leftovers.Items[i].item, leftovers.Items[i].number, *ings)
 						} else {
-							Recipe = AddToSlice(Leftovers.Items[i], Recipe)
+							*recipe = AddItemToSlice(leftovers.Items[i].item, leftovers.Items[i].number, *recipe)
 						}
 					}
 				}
@@ -156,11 +184,10 @@ func handleLeftovers(leftoverSet bool) (bool) {
 
 	leftoverSet = false
 
-
 	if memory.Amt_Correct(1) >= 1 && memory.Amt_Correct(1) < 3 ||
-	memory.Amt_Correct(2) >= 1 && memory.Amt_Correct(2) < 3  {
+		memory.Amt_Correct(2) >= 1 && memory.Amt_Correct(2) < 3 {
 		leftoverSet = true // May have a leftover set
-		Leftovers.DeepCopyMemory(memory)
+		leftovers.DeepCopyMemory(*memory)
 	}
 
 	return leftoverSet
@@ -171,11 +198,16 @@ func AddToSlice(ing string, s []string) []string {
 	return newSlice
 }
 
-func (memory *Memory) AddToMemory(ing string, corState int8) {
+func AddItemToSlice(item string, orderNum int, s []ItemHolder) []ItemHolder {
+	newItem := ItemHolder{item: item, number: orderNum}
+	newSlice := append(s, newItem)
+	return newSlice
+}
+
+func (memory *Memory) AddToMemory(ing string, corState int8, orderNum int) {
 	memory.Items[2] = memory.Items[1]
 	memory.Items[1] = memory.Items[0]
-	memory.Items[0] = ing
-
+	memory.Items[0] = ItemHolder{item: ing, number: orderNum}
 
 	memory.Item_Type[2] = memory.Item_Type[1]
 	memory.Item_Type[1] = memory.Item_Type[0]
@@ -183,10 +215,12 @@ func (memory *Memory) AddToMemory(ing string, corState int8) {
 	memory.Amt++
 }
 
-func search(link string, col *colly.Collector, bModel *bayesian.Classifier) {
-	col.Visit(link)
-	fmt.Println("Ended search")
-	PrintAllInSlice(Ings)
-	fmt.Println("-----------------")
-	PrintAllInSlice(Recipe)
+func sortItemHoldersByOrder(items []ItemHolder) {
+	for i := 0; i < len(items)-1; i++ {
+		for j := 0; j < len(items)-i-1; j++ {
+			if items[j].number > items[j+1].number {
+				items[j], items[j+1] = items[j+1], items[j]
+			}
+		}
+	}
 }
